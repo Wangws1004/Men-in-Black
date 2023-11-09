31fc5555138c045e6fef1e837195a7a49af1c930
syshin0116 <syshin0116@gmail.com>
2023-11-09 10:49:28 +0900

    pre file clean Yolov8, Sort, EasyOCR

--- Automatic License Plate Recognition/main.py
+++ Automatic License Plate Recognition/main.py
@@ -42,7 +42,7 @@ while ret:
     if ret:
         results[frame_nmr] = {}
         # detect vehicles
-        detections = coco_model(frame)[0]
+        detections = coco_model(frame, device='mps')[0]
         detections_ = []
         for detection in detections.boxes.data.tolist():
             x1, y1, x2, y2, score, class_id = detection
@@ -73,17 +73,11 @@ while ret:
                 license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
 
                 if license_plate_text is not None:
-                    results[frame_nmr][car_id] = {
-                        'car': {
-                            'bbox': [xcar1, ycar1, xcar2, ycar2],
-                            # 'class': class_id_dict[class_id]
-                                },
-                        'license_plate': {
-                            'bbox': [x1, y1, x2, y2],
-                            'text': license_plate_text,
-                            'bbox_score': score,
-                            'text_score': license_plate_text_score}
-                    }
+                    results[frame_nmr][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
+                                                  'license_plate': {'bbox': [x1, y1, x2, y2],
+                                                                    'text': license_plate_text,
+                                                                    'bbox_score': score,
+                                                                    'text_score': license_plate_text_score}}
 
 # write results
 write_csv(results, './test.csv')
